352b972cbcd10c9dd4b56538bec9e6d75a821dc9
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-03-21 21:38:43 +0530

    added persist

---
+++ src/store/reducers/todo.ts
@@ -0,0 +1,50 @@
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+
+const initialState = {
+  loading: false,
+  loaded: false,
+};
+
+export default (state = initialState, action: any) => {
+  const {type, payload} = action;
+  const {session} = payload;
+  const {Todo} = session;
+
+  switch (type) {
+    case ADD_TODO: {
+      Todo.create({id: payload.id, name: payload.name});
+      break;
+    }
+
+    case REMOVE_TODO: {
+      Todo.withId(payload.id).delete();
+      break;
+    }
+
+    case TOGGLE_TODO: {
+      Todo.withId(payload.id).update({status: !payload.status});
+      break;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+export const addTodo = (payload: any, session: any) => ({
+  type: ADD_TODO,
+  payload: {...payload, session},
+});
+
+export const removeTodo = (payload: any, session: any) => ({
+  type: REMOVE_TODO,
+  payload: {...payload, session},
+});
+
+export const toggleTodo = (payload: any, session: any) => ({
+  type: TOGGLE_TODO,
+  payload: {...payload, session},
+});
